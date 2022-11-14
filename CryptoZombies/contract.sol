ec308cf3df0f6bfab35814f38c75851ffadae3d4
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-14 23:43:28 +0300

    solved arrays

--- CryptoZombies/contract.sol
+++ CryptoZombies/contract.sol
@@ -14,7 +14,8 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private  {
-        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.length - 1;
         emit NewZombie(id, _name, _dna);
         
     }
