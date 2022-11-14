9fadf2f6fc7ae6f31e13def5dfa1428f7d050298
kkipngenokoech <kkipngenokoech22@gmail.com>
2022-11-14 23:34:37 +0300

    emitinf events proving difficult

--- CryptoZombies/contract.sol
+++ CryptoZombies/contract.sol
@@ -1,6 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 contract ZombieFactory {
+
+    event NewZombie(uint ZombieId, string name, uint dna);
     uint dnaDigits = 16;
     uint dnaModulus = 10**dnaDigits;
 
@@ -12,7 +14,9 @@ contract ZombieFactory {
     Zombie[] public zombies;
 
     function _createZombie(string memory _name, uint _dna) private  {
-        zombies.push(Zombie(_name, _dna));
+        uint id = zombies.push(Zombie(_name, _dna)) - 1;
+        emit NewZombie(id, _name, _dna);
+        
     }
 
     function _generateRandomDna(string memory _str) private view returns(uint) {
---
+++ CryptoZombies/events.md
@@ -0,0 +1,3 @@
+# EVENTS
+
+This is a way through which your contract communicates to your front-end that certain events happened.
